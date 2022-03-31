f618a1031a4b56a7cb5f5850ab3751d321ce25b3
赵Muse <83506132@qq.com>
2022-03-31 19:10:53 +0800

    initial generat

---
+++ demo-instance-project/src/main/java/org/muse/demo/app/ApplicationMain.java
@@ -0,0 +1,18 @@
+package org.muse.demo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * description:
+ *
+ * @Author ZhaoMuse
+ * @date 2022/3/29 14:25
+ * @Since
+ */
+@SpringBootApplication
+public class ApplicationMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationMain.class, args);
+    }
+}
